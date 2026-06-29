0b4bf34a072ca42d1ed0f2106c07197dceff8b1a
AMBDY <109236757+AMBDY@users.noreply.github.com>
2026-06-29 10:38:03 +0100

    Update sb-config.js

--- project/public/js/sb-config.js
+++ project/public/js/sb-config.js
@@ -1,6 +1,6 @@
 // Inject Supabase config and load the client library
-window.SUPABASE_URL = 'https://jvxhtdbcowhsllnwrtut.supabase.co';
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2eGh0ZGJjb3doc2xsbndydHV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0NDY5OTYsImV4cCI6MjA5ODAyMjk5Nn0.jCpv_pgnJ5ZdU-brYh-g78xmXj2gEpvYDhOfE6T3fvo';
+window.SUPABASE_URL = 'https://jyjqgrjqtvkuznpdgjct.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5anFncmpxdHZrdXpucGRnamN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2OTgwODYsImV4cCI6MjA5ODI3NDA4Nn0.SoPB5dmiX-l5tpSJUlww2abqjvd_FyNF6ce7IeqosqE';
 
 // Load supabase-js from CDN (UMD build exposes window.supabaseJs)
 (function () {
